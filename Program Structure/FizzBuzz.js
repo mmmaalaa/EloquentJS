c156dd44e3f2b8b238058f7e8209ce96b04666ab
mmmaalaa <mmmaaalaa13@gmail.com>
2025-07-15 15:50:30 +0300

    Add Program Structure folder

---
+++ Program Structure/FizzBuzz.js
@@ -0,0 +1,22 @@
+// if number is divisible by 3, print "fizz"
+// if number is divisible by 5, print "buzz"
+// if number is divisible by 5 and 3, print "fizzbuzz"
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("fizzbuzz");
+  } else if (i % 3 === 0) {
+    console.log("fizz");
+  } else if (i % 5 === 0) {
+    console.log("buzz");
+  }else {
+    console.log(i)
+  }
+}
+
+for (let n = 1; n <= 100; n++) {
+  let output = "";
+  if (n % 3 == 0) output += "Fizz";
+  if (n % 5 == 0) output += "Buzz";
+  console.log(output || n);
+}
